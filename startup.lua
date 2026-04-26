a4b2bffd643e42bb0182475d412bf0afdd7aeb78
the programmer <88006978+trixalb@users.noreply.github.com>
2026-04-25 21:46:06 -0300

    Add startup.lua for monitor and sensor interface

---
+++ startup.lua
@@ -0,0 +1,15 @@
+m_id = 0
+m = peripheral.wrap("monitor_"..m_id)
+r = peripheral.wrap("bottom")
+
+function map(x,in_min,in_max,out_min,ou_max)
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min)
+end
+
+while true do
+	m.clear()
+	m.setCursorPos(1,1)
+	m.setTextScale(2)
+	m.write("TANQUE: "..math.floor(map(r.getAnalogInput("front"),0,15,0,255)).."%")
+	sleep(1)
+end
